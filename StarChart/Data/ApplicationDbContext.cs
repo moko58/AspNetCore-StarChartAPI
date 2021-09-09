533b5d137036f3827b22c3293b729f100fbb78f3
moko58 <r_chakra@yahoo.com>
2021-09-09 13:10:48 +0530

    module-2-1

--- StarChart/Data/ApplicationDbContext.cs
+++ StarChart/Data/ApplicationDbContext.cs
@@ -5,10 +5,11 @@ namespace StarChart.Data
 {
     public class ApplicationDbContext : DbContext
     {
-        public ApplicationDbContext(DbContextOptions options) : base(options)
-        {
-            public DbSet<CelestialObject> CelestialObjects { get; set; }
-        }
+        public DbSet<CelestialObject> CelestialObjects { get; set; }
+        public ApplicationDbContext(DbContextOptions options)
+        : base(options)
+        { }
+           
     }
 }
 
